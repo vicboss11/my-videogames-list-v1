3dc9afe2c3a33fd9f5a2e90cd8392defa9a04b53
Victor <victor.af1104@gmail.com>
2019-12-04 02:13:47 +0100

    cambios

--- js/script.js
+++ js/script.js
@@ -18,16 +18,113 @@ function drawLibrary() {
         output.innerHTML = "";
 
         videogamesList.forEach((videogame) => {
-            output.innerHTML += '<div class="col-auto my-3">' +
-                '<div class="videogame-box border rounded">' +
-                '<div class="videogame-image">' +
+            output.innerHTML += '<div class="videogame-box border rounded-0 mx-auto mb-3 m-md-1">' +
+                '<div class="videogame-image" data-toggle="modal" data-target="#' + videogame.ID + '">' +
                 '<img src="images/videogames/' + videogame.boxImage + '" alt="Imagen de ' + videogame.name + '">' +
                 '</div>' +
                 '<div class="videogame-info text-dark text-center">' +
-                '<p>' + videogame.name + '</p>' +
+                '<div class="d-flex flex-row justify-content-center">' +
+                '<div class="text-truncate">' +
+                videogame.name +
                 '</div>' +
                 '</div>' +
-                '</div>';
+                '<div class="d-flex flex-row justify-content-around videogame-options pt-2 px-4">' +
+                '<div>' +
+                '<button type="button" class="btn border rounded custom-btn no-focus"' +
+                'data-toggle="modal" data-target="#' + videogame.ID + '" data-tooltip="tooltip" trigger="hover"' +
+                'data-placement="bottom" title="Ver">' +
+                '<i class="far fa-eye"></i>' +
+                '</button>' +
+                '</div>' +
+                '<div>' +
+                '<button type="button" class="btn border rounded custom-btn no-focus"' +
+                'data-toggle="modal" data-target="#remove-videogame-' + videogame.ID + '" data-tooltip="tooltip" trigger="hover" data-placement="bottom" title="Eliminar">' +
+                '<i class="far fa-trash-alt"></i>' +
+                '</button>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="modal fade" id="' + videogame.ID + '" tabindex="-1" role="dialog">' +
+                '<div class="modal-dialog modal-dialog-centered" role="document">' +
+                '<div class="modal-content rounded-0">' +
+                '<div class="modal-header">' +
+                '<h5 class="modal-title title">' + videogame.name + '</h5>' +
+                '<button type="button" class="close custom-btn no-focus" data-dismiss="modal" aria-label="Close">' +
+                '<span aria-hidden="true">&times;</span>' +
+                '</button>' +
+                '</div>' +
+                '<div class="modal-body">' +
+                '<div class="row justify-content-center">' +
+                '<div class="col-12">' +
+                '<strong>Nombre:</strong> ' + videogame.name +
+                '</div>' +
+                '</div>' +
+                '<div class="row justify-content-center">' +
+                '<div class="col-12">' +
+                '<strong>Genero/s:</strong> ' + videogame.genre +
+                '</div>' +
+                '</div>' +
+                '<div class="row justify-content-center">' +
+                '<div class="col-12">' +
+                '<strong>Desarrollador:</strong> ' + videogame.developer +
+                '</div>' +
+                '</div>' +
+                '<div class="row justify-content-center">' +
+                '<div class="col-12">' +
+                '<strong>Editora:</strong> ' + videogame.publisher +
+                '</div>' +
+                '</div>' +
+                '<div class="row justify-content-center">' +
+                '<div class="col-12">' +
+                '<strong>Platamorma/s:</strong> ' + videogame.platform +
+                '</div>' +
+                '</div>' +
+                '<div class="row justify-content-center">' +
+                '<div class="col-12">' +
+                '<strong>PEGI:</strong> ' + videogame.rating +
+                '</div>' +
+                '</div>' +
+                '<div class="row justify-content-center">' +
+                '<div class="col-12 blockquote">' +
+                'En tu colección desde el ' + videogame.sinceDate.getDate() + "-" + (videogame.sinceDate.getMonth() + 1) + "-" + videogame.sinceDate.getFullYear() +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '<div class="modal-footer">' +
+                '<button type="button" class="btn btn-dark rounded-0 no-focus" data-dismiss="modal">' +
+                'Salir' +
+                '</button>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+
+                '<div class="modal fade" id="remove-videogame-' + videogame.ID + '" tabindex="-1" role="dialog">' +
+                '<div class="modal-dialog modal-dialog-centered" role="document">' +
+                '<div class="modal-content rounded-0">' +
+                '<div class="modal-header">' +
+                '<h5 class="modal-title title">ELIMINAR ' + videogame.name + '</h5>' +
+                '<button type="button" class="close custom-btn no-focus" data-dismiss="modal" aria-label="Close">' +
+                '<span aria-hidden="true">&times;</span>' +
+                '</button>' +
+                '</div>' +
+                '<div class="modal-body">' +
+                '<p class="text-center">' +
+                '¿Seguro que quieres eliminar ' + videogame.name + ' de tu lista?' +
+                '</p>' +
+                '</div>' +
+                '<div class="modal-footer">' +
+                '<button type="button" class="btn btn-dark rounded-0 no-focus" data-dismiss="modal">' +
+                'Cancelar' +
+                '</button>' +
+                '<button type="button" class="btn btn-danger rounded-0 no-focus" data-dismiss="modal">' +
+                'Eliminar' +
+                '</button>' +
+                '</div>' +
+                '</div>' +
+                '</div>' +
+                '</div>'
         });
     }
 }
@@ -56,7 +153,15 @@ function removeVideogameByID(videogameID) {
 // Función para borrar toda la lista
 function deleteEntireList() {
     // Para borrar el array se vuelve a instanciar
-    videogamesList = new Array();
+    videogamesList.length = 0;
+}
+
+// ####################################################################################################################
+// => Formularios
+// Función para crear un videojuego a partir de los datos de su formulario correspondiente
+function addNewVideogameFromDataForm() {
+    let name = getElementById('videogame-name').value;
+    addVideogame(new Videogame(name, genre, developer, publisher, platform, rating, image));
 }
 
 // ####################################################################################################################
@@ -65,24 +170,23 @@ function deleteEntireList() {
 // (superclase) de videojuego para hacer uso de la herencia
 // -> Constructor
 function Piece() {
-    this.ID = uuid.v1(); // UUID único
+    this.ID = uuidv1(); // UUID único
 }
 
 // ####################################################################################################################
 // *** clase Videogame
 // {Videojuego} Será una CLASE HIJA (subclase) de Piece, de la cuál heredará un identificador
 // -> Constructor
-function Videogame(name, genre, developer, publisher, platform, releaseDate, rating, image) {
-    Piece.call(this, ID); // Hereda la propiedad ID de Piece
-    this.ID = "VG" + this.ID; // Se sobrescribe el ID heredado añadiendo el String "VG" (VideoGame) al principio
+function Videogame(name, genre, developer, publisher, platform, rating, image) {
+    Piece.call(this); // Hereda la propiedad ID de Piece
+    this.ID = "VG-" + this.ID; // Se sobrescribe el ID heredado añadiendo el String "VG" (VideoGame) al principio
     this.name = new String(name); // nombre
     this.genre = new String(genre); // genero/s
     this.developer = new String(developer); // empresa desarrolladora
     this.publisher = new String(publisher); // empresa editora
     this.platform = new String(platform); // plataforma/s
-    this.releaseDate = new Date(Date.parse(releaseDate)); // fecha de lanzamiento
     this.rating = new String(rating); // calificación de edad
-    this.boxImage = new Image().src(image);
+    this.boxImage = image;
     this.score = null; // valoracion
     this.sinceDate = new Date(); // fecha en la que se añade el videojuego a la biblioteca
 }
